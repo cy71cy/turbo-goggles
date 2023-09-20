1cf46a330a5d2d4130695c8e0341371f9650d998
cy <1658461428@qq.com>
2023-09-20 16:27:46 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <!--<img alt="Vue logo" src="./assets/logo.png">--> 
+ <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div class="nav-bar">
+    <van-nav-bar>
+      <div class="title">个人基本信息</div>
+    </van-nav-bar>
+  </div>
+
+  <div class="personal-page">
+    <img :src="one" alt="One">
+    <h2>{{ sname }}</h2>
+    <p>{{ id }}</p>
+  </div>   
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      one:"../Images/one.jpg",
+      sname:"chenyu",
+      id:"2310273036"
+    };
   }
-}
+ // name: 'App',
+ // components: {
+ //   HelloWorld
+//  }
+};
 </script>
 
 <style>
---
+++ src/newFile.js
@@ -0,0 +1,13 @@
+export default (await import('vue')).defineComponent({
+data() {
+return {
+one: "../Images/one.jpg",
+name: "chenyu",
+id: "2310273036"
+};
+},
+name: 'App',
+components: {
+HelloWorld
+}
+});
